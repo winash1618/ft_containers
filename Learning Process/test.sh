66e15b0696d92db9e4465140fbd861a221be2041
Muhammed Asharaf K V <51042907+winash1618@users.noreply.github.com>
2022-08-26 13:38:30 +0000

    make file changes

--- Learning Process/test.sh
+++ Learning Process/test.sh
@@ -6,7 +6,7 @@ RED='\033[1;31m'
 GREEN='\033[1;32m'
 YELLOW='\033[0;33m'
 CYAN='\033[0;36m'
-
+cd ./exe
 echo -e "${YELLOW}Redirecting ft_container to ft${DEFAULT}"
 ./ft_container > ft
 echo -e "${YELLOW}Redirecting std_container to std${DEFAULT}"
@@ -19,6 +19,6 @@ if [ -s final ]; then
 else
 	echo -e "${GREEN}PASS 125${DEFAULT}"
 fi
-
+cd ../
 rm ft std final
 make fclean
