0f3d4decc993361482e4a566340eb4cbd5d96969
Muhammed Asharaf K V <51042907+winash1618@users.noreply.github.com>
2022-08-28 10:51:31 +0000

    added clear, front, getallocator tests and working

---
+++ tests/v_clear_test.cpp
@@ -0,0 +1,33 @@
+#include "vector.hpp"
+#include "iterator.hpp"
+#include "reverse_iterator.hpp"
+#include <iterator>
+#include <vector>
+#include <map>
+#include <utility>
+
+
+int main ()
+{
+  std::vector<int> myvector;
+  myvector.push_back (100);
+  myvector.push_back (200);
+  myvector.push_back (300);
+
+  std::cout << "myvector contains:";
+  for (unsigned i=0; i<myvector.size(); i++)
+    std::cout << ' ' << myvector[i];
+  std::cout << '\n';
+
+  myvector.clear();
+  myvector.push_back (1101);
+  myvector.push_back (2202);
+
+  std::cout << "myvector contains:";
+  for (unsigned i=0; i<myvector.size(); i++)
+    std::cout << ' ' << myvector[i];
+  std::cout << '\n';
+std::cout << "size: " << (int) myvector.size() << '\n';
+  std::cout << "capacity: " << (int) myvector.capacity() << '\n';
+  return 0;
+}
---
+++ tests/v_front_test.cpp
@@ -0,0 +1,25 @@
+#include "vector.hpp"
+#include "iterator.hpp"
+#include "reverse_iterator.hpp"
+#include <iterator>
+#include <vector>
+#include <map>
+#include <utility>
+
+
+int main ()
+{
+  std::vector<int> myvector;
+
+  myvector.push_back(78);
+  myvector.push_back(16);
+
+  // now front equals 78, and back 16
+
+  myvector.front() -= myvector.back();
+
+  std::cout << "myvector.front() is now " << myvector.front() << '\n';
+std::cout << "size: " << (int) myvector.size() << '\n';
+  std::cout << "capacity: " << (int) myvector.capacity() << '\n';
+  return 0;
+}
