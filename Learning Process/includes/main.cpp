bd19c833e03426b4a7836ed5236d11a85e39147f
Muhammed Asharaf K V <51042907+winash1618@users.noreply.github.com>
2022-08-28 22:35:55 +0000

    ft::pair relational operator working with main.cpp in includes

--- Learning Process/includes/main.cpp
+++ Learning Process/includes/main.cpp
@@ -1,17 +1,35 @@
-#include "pair.hpp"      // std::pair, std::make_pair
-#include <string>       // std::string
-#include <iostream>     // std::cout
+// #include "pair.hpp"      // std::pair, std::make_pair
+// #include <string>       // std::string
+// #include <iostream>     // std::cout
 
-int main () {
-  ft::pair <std::string,int> planet;
-  ft::pair <std::string,int> homeplanet;
+// int main () {
+//   ft::pair <std::string,int> planet;
+//   ft::pair <std::string,int> homeplanet;
   
 
-  planet = ft::make_pair("Earth",6371);
+//   planet = ft::make_pair("Earth",6371);
 
-  homeplanet = planet;
-	ft::pair <std::string,int> exoplanet(homeplanet);
-  std::cout << "Home planet: " << exoplanet.first << '\n';
-  std::cout << "Planet size: " << exoplanet.second << '\n';
+//   homeplanet = planet;
+// 	ft::pair <std::string,int> exoplanet(homeplanet);
+//   std::cout << "Home planet: " << exoplanet.first << '\n';
+//   std::cout << "Planet size: " << exoplanet.second << '\n';
+//   return 0;
+// }
+#include "pair.hpp"
+// #include <utility>      // std::pair
+#include <iostream>     // std::cout
+
+int main ()
+{
+  ft::pair<int,char> foo (10,'z');
+  ft::pair<int,char> bar (90,'a');
+
+  if (foo==bar) std::cout << "foo and bar are equal\n";
+  if (foo!=bar) std::cout << "foo and bar are not equal\n";
+  if (foo< bar) std::cout << "foo is less than bar\n";
+  if (foo> bar) std::cout << "foo is greater than bar\n";
+  if (foo<=bar) std::cout << "foo is less than or equal to bar\n";
+  if (foo>=bar) std::cout << "foo is greater than or equal to bar\n";
+
   return 0;
 }
