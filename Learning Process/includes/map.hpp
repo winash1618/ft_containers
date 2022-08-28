8b63145d549bb67eea7bd3e2e991b2a8f1320db4
Muhammed Asharaf K V <51042907+winash1618@users.noreply.github.com>
2022-08-28 23:08:59 +0000

    started map.hpp

--- Learning Process/includes/map.hpp
+++ Learning Process/includes/map.hpp
@@ -1,9 +1,45 @@
 #ifndef MAP_HPP
 # define MAP_HPP
-
+# include "pair.hpp"
+# include <iostream>
+# include <algorithm>
+# include <functional>
+# include "iterator.hpp"
+# include "iterator_traits.hpp"
+# include "reverse_iterator.hpp"
 namespace ft
 {
+	template < class Key,                                     // map::key_type
+	class T,                                       // map::mapped_type
+	class Compare = std::less<Key>,                     // map::key_compare
+	class Alloc = std::allocator<ft::pair<const Key,T> >    // map::allocator_type
+	> class map
+	{
+		public:
+			typedef Key													key_type;
+			typedef T													mapped_type;
+			typedef ft::pair<const Key, T>								value_type;
+			typedef Compare												key_compare;
+			typedef Alloc												allocator_type;
+			typedef typename allocator_type::reference					reference;
+			typedef typename allocator_type::const_reference			const_reference;
+			typedef typename allocator_type::pointer					pointer;
+			typedef typename allocator_type::const_pointer				const_pointer;
+			typedef ft::__wrap_iter<value_type>							iterator;
+			typedef ft::__wrap_iter<value_type>							const_iterator;
+			typedef ft::reverse_iterator<iterator>						reverse_iterator;
+			typedef ft::reverse_iterator<const_iterator>				const_reverse_iterator;
+			typedef ft::iterator_traits<iterator>::difference_type		difference_type;
+			typedef typename std::size_t								size_type;
 
-	
+			explicit map (const key_compare& comp = key_compare(),
+              const allocator_type& alloc = allocator_type()){}
+			template <class InputIterator>
+			map (InputIterator first, InputIterator last,
+				const key_compare& comp = key_compare(),
+				const allocator_type& alloc = allocator_type());	
+			map (const map& x);
+	};
+
 }
 #endif
