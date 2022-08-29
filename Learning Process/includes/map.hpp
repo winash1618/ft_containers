edd2f632c66c1d2ab25d0f240cfef1a046dd4eab
Muhammed Asharaf K V <51042907+winash1618@users.noreply.github.com>
2022-08-29 02:25:10 +0000

    member function prototype added

--- Learning Process/includes/map.hpp
+++ Learning Process/includes/map.hpp
@@ -33,13 +33,103 @@ namespace ft
 			typedef typename std::size_t								size_type;
 
 			explicit map (const key_compare& comp = key_compare(),
-              const allocator_type& alloc = allocator_type()){}
+			const allocator_type& alloc = allocator_type()){}
 			template <class InputIterator>
 			map (InputIterator first, InputIterator last,
 				const key_compare& comp = key_compare(),
 				const allocator_type& alloc = allocator_type());	
 			map (const map& x);
+
+			~map();
+
+			iterator begin();
+			const_iterator begin() const;
+
+			void clear();
+
+			size_type count (const key_type& k) const;
+
+			bool empty() const;
+
+			iterator end();
+			const_iterator end() const;
+
+			pair<const_iterator,const_iterator> equal_range (const key_type& k) const;
+			pair<iterator,iterator>             equal_range (const key_type& k);
+
+				
+			void erase (iterator position);
+			size_type erase (const key_type& k);
+			void erase (iterator first, iterator last);
+
+			iterator find (const key_type& k);
+			const_iterator find (const key_type& k) const;
+
+			allocator_type get_allocator() const;
+
+			// single element insert
+			pair<iterator,bool> insert (const value_type& val);
+			// with hint insert	
+			iterator insert (iterator position, const value_type& val);
+			// range insert
+			template <class InputIterator>
+			void insert (InputIterator first, InputIterator last);
+
+
+			key_compare key_comp() const;
+
+
+			iterator lower_bound (const key_type& k);
+			const_iterator lower_bound (const key_type& k) const;
+
+			size_type max_size() const;
+
+			mapped_type& operator[] (const key_type& k);
+
+			map& operator= (const map& x);
+
+
+			reverse_iterator rbegin();
+			const_reverse_iterator rbegin() const;
+
+
+			reverse_iterator rend();
+			const_reverse_iterator rend() const;
+
+			size_type size() const;
+
+			void swap (map& x);
+
+			iterator upper_bound (const key_type& k);
+			const_iterator upper_bound (const key_type& k) const;
+
+			value_compare value_comp() const;
 	};
+		template <class Key, class T, class Compare, class Alloc>
+		bool operator== ( const map<Key,T,Compare,Alloc>& lhs,
+							const map<Key,T,Compare,Alloc>& rhs );
+		
+		template <class Key, class T, class Compare, class Alloc>
+		bool operator!= ( const map<Key,T,Compare,Alloc>& lhs,
+							const map<Key,T,Compare,Alloc>& rhs );
+			
+		template <class Key, class T, class Compare, class Alloc>
+		bool operator<  ( const map<Key,T,Compare,Alloc>& lhs,
+							const map<Key,T,Compare,Alloc>& rhs );
+		
+		template <class Key, class T, class Compare, class Alloc>
+		bool operator<= ( const map<Key,T,Compare,Alloc>& lhs,
+							const map<Key,T,Compare,Alloc>& rhs );
+			
+		template <class Key, class T, class Compare, class Alloc>
+		bool operator>  ( const map<Key,T,Compare,Alloc>& lhs,
+							const map<Key,T,Compare,Alloc>& rhs );
+		
+		template <class Key, class T, class Compare, class Alloc>
+		bool operator>= ( const map<Key,T,Compare,Alloc>& lhs,
+							const map<Key,T,Compare,Alloc>& rhs );
+		template <class Key, class T, class Compare, class Alloc>
+		void swap (map<Key,T,Compare,Alloc>& x, map<Key,T,Compare,Alloc>& y);
 
 }
 #endif
